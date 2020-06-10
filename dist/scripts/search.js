66a727082cc95fd4e0169bf4c0b5782b8e5a1453
abhishek-designs <akalyane980@gmail.com>
2020-06-10 22:36:32 +0530

    Latest Commit

--- dist/scripts/search.js
+++ dist/scripts/search.js
@@ -1,32 +1,36 @@
-// grabbing the controls
-const srchBtn = document.querySelector('.srch-btn');
-const closeBtn = document.querySelector('.cancel-btn');
+for(let i=0; i<2; i++)
+{
 
-// grabbing the search box
-const searchBox = document.querySelector('.search');
-
-// adding functionality to the controls
-srchBtn.addEventListener('click',function(){
-
-    // toggle settings
-    if(searchBox.style.transform == 'translateY(-300px)')
-    {
-        searchBox.style.transform = 'translateY(0)';
-        // searchBox.style.position = 'absolute';
-        this.classList.add('selected');
-    }
-    else
-    {
+    // grabbing the controls
+    const srchBtn = document.querySelectorAll('.srch-btn')[i];
+    const closeBtn = document.querySelector('.cancel-btn');
+    
+    // grabbing the search box
+    const searchBox = document.querySelector('.search');
+    
+    // adding functionality to the controls
+    srchBtn.addEventListener('click',function(){
+    
+        // toggle settings
+        if(searchBox.style.transform == 'translateY(-300px)')
+        {
+            searchBox.style.transform = 'translateY(0)';
+            // searchBox.style.position = 'absolute';
+            this.classList.add('selected');
+        }
+        else
+        {
+            searchBox.style.transform = 'translateY(-300px)';
+            this.classList.remove('selected');
+        }
+        // console.log('hello');
+    });
+    
+    closeBtn.addEventListener('click',function(){
+        if(srchBtn.classList == 'selected')
+        {
+            srchBtn.classList.remove('selected');
+        }
         searchBox.style.transform = 'translateY(-300px)';
-        this.classList.remove('selected');
-    }
-    // console.log('hello');
-});
-
-closeBtn.addEventListener('click',function(){
-    if(srchBtn.classList == 'selected')
-    {
-        srchBtn.classList.remove('selected');
-    }
-    searchBox.style.transform = 'translateY(-300px)';
-});
+    });
+}
