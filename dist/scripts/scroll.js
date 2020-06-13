44b4896327830bdeace3decf3b11a5c8f09e1596
abhishek-designs <akalyane980@gmail.com>
2020-06-13 14:55:24 +0530

    Latest Commit

--- dist/scripts/scroll.js
+++ dist/scripts/scroll.js
@@ -1,18 +1,67 @@
-const navbar = document.querySelector('#navbar');
-const searchBox = document.querySelector('.search');
+// const navbar = document.querySelector('#navbar');
+// const searchBox = document.querySelector('.search');
 
-let scrolled = false;
+// let scrolled = false;
 
-window.onscroll = function()
+// window.onscroll = function()
+// {
+//     if(window.pageYOffset > 100)
+//     {
+//         navbar.style.background = 'rgba(255,255,255,0.95)';
+//     }
+//     else
+//     {
+//         navbar.style.background = '#fff';
+//     }
+//     // console.log('scrolled');
+
+// }
+
+const toggle = document.querySelector('.toggler');
+toggle.addEventListener('click',onChecked);
+
+
+var checked = false;
+
+function onChecked()
 {
-    if(window.pageYOffset > 100)
+    if(checked == false)
     {
-        navbar.style.background = 'rgba(255,255,255,0.95)';
+        checked = true;
     }
     else
     {
-        navbar.style.background = '#fff';
+        checked = false;
     }
-    // console.log('scrolled');
 
+    this.checked = checked;
+
+    if(this.checked)
+    {
+        console.log('checked');
+        disableScroll();
+    }
+    else
+    {
+        console.log('unchecked');
+        enableScroll();
+    }
+}
+
+// const screen = document.body.style.overflow;
+
+
+
+// Control scrolling
+window.addEventListener('scroll',disableScroll);
+window.addEventListener('scroll',enableScroll);
+
+function disableScroll()
+{
+    document.body.style.overflow = "hidden";
+}
+
+function enableScroll()
+{
+    document.body.style.overflow = "auto";
 }
